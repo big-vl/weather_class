7844ddedbf5379635042aa26280f5dd8e2fc5e29
big-vl <big-vl@ya.ru>
2020-08-15 00:47:18 +0300

    add direction of the wind (f)

--- weather.py
+++ weather.py
@@ -24,22 +24,19 @@ class Weather:
         try:
             if len(str(city_name_)) < 3:
                 city_name_ = 'Moscow'
-            self.settings = {
-                            'q': city_name_,
-                            'appid': self.api_key,
-                            'units': metric
-                            }
+            self.settings = {'q': city_name_,
+                             'appid': self.api_key,
+                             'units': metric}
             response = requests.get(self.server, params=self.settings)
             self.weather_data = json.loads(response.text)
+            print(self.weather_data)
             self.set_settings()
             self.set_city_name(city_name_)
             self.set_wind_setting()
         except IOError as error:
             print('Class Weather Error connect:', error)
-            self.weather_data = {
-                                 'cod': '0000',
-                                 'message': error
-                                }
+            self.weather_data = {'cod': '0000',
+                                 'message': error}
 
     def set_city_name(self, _city_name):
         self.city_name = _city_name
@@ -61,17 +58,15 @@ class Weather:
     def content(self, format_time='%Y-%m-%d %H:%M:%S'):
         if (len(self.weather_data) != 0) and (
                               'message' not in self.weather_data):
-            _content = {
-                        'app_name': self.app_name,
+            _content = {'app_name': self.app_name,
                         'city': self.city(),
                         'temp': str(self.temp()) + self.temp_settings,
                         'pressure': self.pressure(),
                         'speed_wind': str(
                                  self.speed_wind()) + self.wind_setting,
                         'date': self.timestamp(format_time),
-                        'city': self.city(),
-                        'ai': self.ai()
-                        }
+                        'deg_wind': str(self.deg_wind()),
+                        'ai': self.ai()}
             return _content
         elif ('message' in self.weather_data):
             return self.error_log()
@@ -82,28 +77,21 @@ class Weather:
                     debug_message = self.weather_data['message']
                 else:
                     debug_message = 'pleas debug True more information'
-                return {
-                        'app_name': self.app_name,
+                return {'app_name': self.app_name,
                         'message': 'Error connect',
-                        'cod': debug_message
-                       }
+                        'cod': debug_message}
             if self.weather_data['cod'] == '404':
-                return {
-                        'app_name': self.app_name,
+                return {'app_name': self.app_name,
                         'message': 'City not found',
-                        'cod': self.weather_data['cod']
-                       }
+                        'cod': self.weather_data['cod']}
             else:
-                return {
-                        'app_name': self.app_name,
+                return {'app_name': self.app_name,
                         'message': 'Error content',
-                        'cod': self.weather_data['cod']
-                        }
+                        'cod': self.weather_data['cod']}
 
     def ai(self):
         temp = int(self.temp())
-        ai_answer = {
-                     -60: 'Наступил ледниковый период',
+        ai_answer = {-60: 'Наступил ледниковый период',
                      -30: 'Даже медведям холодно:)',
                      -25: 'Оденьте шубу',
                      -15: 'Нужна телогрейка',
@@ -113,8 +101,7 @@ class Weather:
                      18: 'Прохладно, лучше утеплится',
                      25: 'Шорты и майка ваша одежда',
                      30: 'Жара выходите в плавках:)',
-                     68: 'Пустыня Деште-Лут, на юго-востоке Ирана',
-                    }
+                     68: 'Пустыня Деште-Лут, на юго-востоке Ирана'}
         number = (min((abs(n-temp), n) for n in ai_answer.keys())[1])
         return ai_answer[number]
 
@@ -123,6 +110,14 @@ class Weather:
 
     def pressure(self):
         return self.weather_data['main']['pressure']
+
+    def deg_wind(self):
+        _wind = self.weather_data['wind']['deg']
+        _tend = ['Север', 'Северо-Восток', 'Восток',
+                'Юго-Восток', 'Юг', 'Юго-Запад',
+                'Запад', 'Северо-Запад']
+        _index = round((_wind/8)/5,625)
+        return _tend[int(_index)]
 
     def speed_wind(self):
         return self.weather_data['wind']['speed']
@@ -141,7 +136,7 @@ def mini_weather():
     app_name = 'Weather Mini'
     city_name = 'Krasnodar'
     api_key = 'a5f756f97a8cf1082787e8d36699c449'
-    server = 'http://api.opnweathermap.org/data/2.5/weather'
+    server = 'http://api.openweathermap.org/data/2.5/weather'
     weather = Weather(app_name, api_key, server, city_name, debug=True)
     if request.method == 'POST':
         city_name = request.form['city']
